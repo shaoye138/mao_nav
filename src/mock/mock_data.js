bebaf8b9bd5c9e378137d9c345c96a324b0091bf
shaoye138 <shaoye138@gmail.com>
2025-08-06 05:56:36 +0800

    chore: 更新导航数据 - 2025/8/6 05:56:37

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -159,7 +159,7 @@ export const mockData = {
           "id": "site-1754365570247",
           "name": "索菲贷",
           "url": "http://www.sofi.com/",
-          "description": "最高100K 未知信用分\n（很快额度高 贷款类型多种 ）\n密西西比州地区受限 首选",
+          "description": "最高100K 未知信用分\n\n（很快额度高 贷款类型多种 ）\n密西西比州地区受限 首选",
           "icon": "/sitelogo/www.sofi.com.ico"
         },
         {
