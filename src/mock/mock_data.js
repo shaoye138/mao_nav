01c97e655be93de8d893e95aca4d8cd71df4c784
shaoye138 <shaoye138@gmail.com>
2025-08-05 12:02:45 +0800

    chore: 更新导航数据 - 2025/8/5 12:02:47

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -174,6 +174,192 @@ export const mockData = {
           "url": "https://oportun.com/ ",
           "description": "最高10K 650信用评分\n（不需要SSN,不是美国身份也可以） \n不良信用贷款",
           "icon": "/sitelogo/oportun.com.ico"
+        },
+        {
+          "id": "site-1754365824173",
+          "name": "信用合作社贷",
+          "url": "https://www.penfed.org/ ",
+          "description": "最高50K 650信用评分",
+          "icon": "/sitelogo/www.penfed.org.ico"
+        },
+        {
+          "id": "site-1754365854322",
+          "name": "升级贷",
+          "url": "https://www.upgrade.com/",
+          "description": "最高50K \n（目前没有透露信用评分） 不良信用贷款",
+          "icon": "/sitelogo/www.upgrade.com.ico"
+        },
+        {
+          "id": "site-1754365882950",
+          "name": "光流贷",
+          "url": "https://www.lightstream.com/",
+          "description": "最高100K 670信用评分 \n家居装修用途易下款 优秀信用记录的最佳贷款",
+          "icon": "/sitelogo/www.lightstream.com.ico"
+        },
+        {
+          "id": "site-1754365905905",
+          "name": "马库斯贷",
+          "url": "https://www.marcus.com/us/en/login",
+          "description": "最高100K 670信用评分",
+          "icon": "/sitelogo/www.marcus.com.ico"
+        },
+        {
+          "id": "site-1754365927823",
+          "name": "付清贷",
+          "url": "https://payoff.com/",
+          "description": "最高40K 600信用评分",
+          "icon": "/sitelogo/payoff.com.ico"
+        },
+        {
+          "id": "site-1754365947474",
+          "name": " AXOS贷",
+          "url": "https://www.axosbank.com/",
+          "description": "最高50K 720信用评分",
+          "icon": "/sitelogo/www.axosbank.com.ico"
+        },
+        {
+          "id": "site-1754365967997",
+          "name": "自由加贷",
+          "url": "https://www.freedomplus.com/ ",
+          "description": "最高50K 670信用评分",
+          "icon": "/sitelogo/www.freedomplus.com.ico"
+        },
+        {
+          "id": "site-1754365989926",
+          "name": "学生小额贷",
+          "url": "https://www.koramoney.com/ ",
+          "description": "最高几K 无需信用评分\n（需要是学生）",
+          "icon": "/sitelogo/www.koramoney.com.ico"
+        },
+        {
+          "id": "site-1754366072658",
+          "name": "卡密诺金融",
+          "url": "https://www.caminofinancial.com/",
+          "description": "支持只有 ITIN或小白\n无信用记录企业）最高额度不详 加州客户较容易批"
+        },
+        {
+          "id": "site-1754366091774",
+          "name": "月桂贷",
+          "url": "https://www.laurelroad.com/personal-loan/ ",
+          "description": "最高45K 信用分不详",
+          "icon": "/sitelogo/www.laurelroad.com.ico"
+        },
+        {
+          "id": "site-1754366110497",
+          "name": "火箭贷",
+          "url": "https://ikiwi.com.mx/prestamos/cohete/",
+          "description": "最高2K 申请快支持全美国18~65岁 容易批",
+          "icon": "/sitelogo/ikiwi.com.mx.ico"
+        },
+        {
+          "id": "site-1754366130753",
+          "name": "先锋贷",
+          "url": "https://www.avant.com/",
+          "description": "最高35K 征信不好可以申请",
+          "icon": "/sitelogo/www.avant.com.ico"
+        },
+        {
+          "id": "site-1754366159235",
+          "name": "德克萨斯州信用贷",
+          "url": "https://www.textelcu-spanish.org/",
+          "description": "最高大概30K 只限德州使用"
+        },
+        {
+          "id": "site-1754366190602",
+          "name": "黑贷 海员金融贷 ",
+          "url": "https://www.marinerfinance.com/ ",
+          "description": "最高25K 征信不好可以贷 \n0信用评分（一些大城市地区受限制）",
+          "icon": "/sitelogo/www.marinerfinance.com.ico"
+        },
+        {
+          "id": "site-1754366210378",
+          "name": "黑贷 不良信用贷",
+          "url": "https://www.badcreditloans.com/",
+          "description": "最高10K 0信用评分",
+          "icon": "/sitelogo/www.badcreditloans.com.ico"
+        },
+        {
+          "id": "site-1754366234587",
+          "name": "鸡蛋学生贷",
+          "url": "https://www.bestegg.com/ ",
+          "description": " 最高250K 建议700信用评分以上\n（目前没有透露）",
+          "icon": "/sitelogo/www.bestegg.com.ico"
+        },
+        {
+          "id": "site-1754366267362",
+          "name": "认真贷",
+          "url": "https://www.earnest.com/",
+          "description": "最高250K  不良信用贷款\n（目前没有透露信用评分）",
+          "icon": "/sitelogo/www.earnest.com.ico"
+        },
+        {
+          "id": "site-1754366289566",
+          "name": "美国银行融资抵押贷",
+          "url": "https://www.usbank.com/",
+          "description": "最高50K 660信用评分",
+          "icon": "/sitelogo/www.usbank.com.ico"
+        },
+        {
+          "id": "site-1754366309582",
+          "name": "道明银行贷",
+          "url": "http://www.dtbank.com/ ",
+          "description": "最高50K 680信用评分 不良信用贷款",
+          "icon": "/sitelogo/www.dtbank.com.ico"
+        },
+        {
+          "id": "site-1754366328118",
+          "name": "发现贷",
+          "url": "https://www.discover.com/",
+          "description": "最高35K 660信用评分",
+          "icon": "/sitelogo/www.discover.com.ico"
+        },
+        {
+          "id": "site-1754366347012",
+          "name": "借贷点",
+          "url": "https://www.lendingpoint.com/ ",
+          "description": "最高40K 580信用评分",
+          "icon": "/sitelogo/www.lendingpoint.com.ico"
+        },
+        {
+          "id": "site-1754366374942",
+          "name": "海军联邦信用合作社",
+          "url": "https://www.navyfederal.org/",
+          "description": "最高50K （必须是信用合作社成员）\n信用评分不限制 ",
+          "icon": "/sitelogo/www.navyfederal.org.ico"
+        },
+        {
+          "id": "site-1754366398387",
+          "name": "加州医疗医护保健专业人员",
+          "url": "https://global.prod.onfiona.com/",
+          "description": "最高250K 信用评分极好"
+        },
+        {
+          "id": "site-1754366426365",
+          "name": "美国现金贷",
+          "url": "https://www.sfgate.com/ ",
+          "description": "最高10K 只要是美国公民或者永久居民即可申请",
+          "icon": "/sitelogo/www.sfgate.com.ico"
+        },
+        {
+          "id": "site-1754366458313",
+          "name": "个人贷款贷",
+          "url": "https://personalloans.com/",
+          "description": "最高35K \n结婚，家居装修，个人贷款不错的选择",
+          "icon": "/sitelogo/personalloans.com.ico"
+        },
+        {
+          "id": "site-1754366505192",
+          "name": "执行贷",
+          "url": "https://www.peerform.com/",
+          "description": " 最高25K 600信用评分 \n有五个州不支持（没有透露哪个州）",
+          "icon": "/sitelogo/www.peerform.com.ico"
+        },
+        {
+          "id": "site-1754366535375",
+          "name": "小企业贷款 ",
+          "url": "https://www.fundingcircle.com/us/ ",
+          "description": "最高500K 660信用评分 \n条件：（有三年以上的企业，内华达州企业受限制）",
+          "icon": "/sitelogo/www.fundingcircle.com.ico"
         }
       ]
     },
